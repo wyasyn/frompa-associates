631617cffe2fd293e96d353e89974f66205f2aef
Yasin Walum <ywalum@gmail.com>
2025-05-29 06:30:48 +0300

    prioritize dark

--- components/logo.tsx
+++ components/logo.tsx
@@ -9,7 +9,7 @@ import { useTheme } from "next-themes";
 export default function Logo() {
   const { theme } = useTheme();
   return (
-    <Link href={"/"}>{theme === "light" ? <DarkLogo /> : <LightLogo />}</Link>
+    <Link href={"/"}>{theme === "dark" ? <LightLogo /> : <DarkLogo />}</Link>
   );
 }
 
